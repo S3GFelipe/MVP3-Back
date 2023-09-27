7352768a59d0d6a42af9fd0a54d9cfc80f022425
S3GFelipe <122311328+S3GFelipe@users.noreply.github.com>
2023-09-27 19:57:40 -0300

    Delete api directory

--- api/schemas/comentario.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-
-class ComentarioSchema(BaseModel):
-    """ Define como um novo comentário a ser inserido deve ser representado
-    """
-    carregador_id: int = 1
-    texto: str = "Carregador confirmado!"
